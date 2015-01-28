a106b65a96802fc6dc00b34a6dc1d54d5506243b
Nick Angelov <nangelov1990@gmail.com>
2015-01-28 20:44:16 +0200

    First Publish

---
+++ Exception Handling/Enter Numbers/NumberValidator.cs
@@ -0,0 +1,86 @@
+﻿namespace Enter_Numbers
+{
+    using System;
+    static class NumberValidator
+    {
+        static void Main()
+        {
+            var start = 1;
+            const int end = 100;
+
+            Console.WriteLine(
+                "Please enter 10 number is the range [{0}..{1}]:",
+                start,
+                end);
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    Console.Write(
+                        "No. {0} --> ",
+                        (i + 1).ToString("D2"));
+
+                    start = ReadNumber(start, end);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.Error.WriteLine(
+                        "The number you entered is not in the range [{0}..{1}]. Please try again!",
+                        start,
+                        end);
+
+                    i = i - 1;
+                }
+                catch (FormatException)
+                {
+                    Console.Error.WriteLine(
+                       "The number you entered is not valid. Please enter a number in the range [{0}..{1}]!",
+                       start,
+                       end);
+
+                    i = i - 1;
+                }
+                catch (OverflowException)
+                {
+                    Console.Error.WriteLine(
+                       "The number you entered is too big. Please enter a number in the range [{0}..{1}]!",
+                       start,
+                       end);
+
+                    i = i - 1;
+                }
+            }
+
+            Console.WriteLine(
+                "End of program. Bye bye!");
+        }
+
+        static int ReadNumber(int start, int end)
+        {
+            Console.Write(
+                "Please enter a number in the range [{0}..{1}]: ",
+                start,
+                end);
+
+            var consoleReader = Console.ReadLine();
+            var number = 0;
+
+            if (!string.IsNullOrEmpty(consoleReader))
+            {
+                number = Int32.Parse(consoleReader);
+
+                if (number < start || number > end)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+
+            return number;
+        }
+    }
+}
---
+++ Exception Handling/Square Root/SimpleCalculator.cs
@@ -0,0 +1,49 @@
+﻿namespace Square_Root
+{
+    using System;
+
+    static class SimpleCalculator
+    {
+        static void Main()
+        {
+            try
+            {
+                var consoleReader = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(consoleReader))
+                {
+                    var number = Int32.Parse(consoleReader);
+                    var answer = CalculateSquareRoot(number);
+                    Console.WriteLine(answer);
+                }
+            }
+            catch (FormatException formatException)
+            {
+                Console.Error.WriteLine(
+                    "Invalid number!");
+            }
+            catch (ArithmeticException arithmeticException)
+            {
+                Console.Error.WriteLine(arithmeticException.Message);
+            }
+            finally
+            {
+                Console.WriteLine(
+                    "Good bye");
+            }
+        }
+
+        static double CalculateSquareRoot(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArithmeticException(
+                    "Negative number!");
+            }
+
+            var output = Math.Sqrt(number);
+
+            return output;
+        }
+    }
+}
